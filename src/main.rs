bd56bcce4f0cd87db6731b32f7b223387a3c9c41
FreakyDude <developer@freakydu.de>
2022-03-10 22:43:39 +0100

    Hello World p2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use bevy::prelude::*;
+
+// #[derive(Component)]
+// struct Position { x: f32, y: f32 }
+
+// fn print_position_system(query: Query<&Transform>) {
+//     for transform in query.iter() {
+//         println!("position: {:?}", transform.translation);
+//     }
+// }
+
+// struct Entity(u64);
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn().insert(Person).insert(Name("Elaina Proctor".to_string()));
+    commands.spawn().insert(Person).insert(Name("Renzo Hume".to_string()));
+    commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
 }
+
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
+
+
+
+fn main() {
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people)
+        .run();
+}
+
